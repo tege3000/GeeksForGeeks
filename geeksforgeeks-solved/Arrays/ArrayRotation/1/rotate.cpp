06b78a90b7024909b79acc26bf8a1326f78daeb1
Tito <teetoegeonu@gmail.com>
2020-07-18 17:28:17 +0100

    UPD - corrected file type mistake for rotate.java

---
+++ geeksforgeeks-solved/Arrays/ArrayRotation/1/rotate.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+// program for array rotation
+void rotate(int arr[], int d, int n) {
+    int el;
+    for(int i = 0; i < d; i++) {
+        el = arr[0];
+        
+        for(int i = 0; i < n-1; i++) {
+            arr[i] = arr[i+1];
+        }
+        
+        arr[n-1] = el;
+    }
+    
+    for(int i = 0; i < n; i ++) {
+        cout << arr[i] << " ";
+    }
+    
+}
+
+int main()
+{
+    int d, n;
+    int arr [] =  {1,2,3,4, 5, 6, 7};
+    n = sizeof(arr)/sizeof(arr[0]);
+    d = 2; // d measures how far to rotate the array
+    rotate(arr, d, n);
+    
+    
+    return 0;
+}
