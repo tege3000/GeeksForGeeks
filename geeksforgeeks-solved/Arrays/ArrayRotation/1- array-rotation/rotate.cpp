797087add0a8885cf06f3f26ca002a7a10c19917
Tito <teetoegeonu@gmail.com>
2020-07-27 12:31:27 +0100

    ADD - Added another implementation for left rotating an array. This one has a better time complexity

--- geeksforgeeks-solved/Arrays/ArrayRotation/1- array-rotation/rotate.cpp
+++ geeksforgeeks-solved/Arrays/ArrayRotation/1- array-rotation/rotate.cpp
@@ -7,24 +7,45 @@
 
 #include <iostream>
 
-using namespace std;
-void rotate(int arr[], int d, int n) {
-    int el;
-    for(int i = 0; i < d; i++) {
-        el = arr[0];
-        
-        for(int i = 0; i < n-1; i++) {
-            arr[i] = arr[i+1];
-        }
-        
-        arr[n-1] = el;
-    }
-    
+void printArray(int arr[], int n) {
     for(int i = 0; i < n; i ++) {
         cout << arr[i] << " ";
     }
+}
+
+
+// second attempt. Works in O(n) time
+void rotate(int arr[], int n, int k) {
+    int newArr[n];
+    for(int i = 0; i < n; i++) {
+        newArr[i] = arr[(k+i)%n];
+    }
+    
+    for(int i = 0; i < n; i++) {
+        arr[i] = newArr[i];
+    }
     
 }
+
+
+
+// // Initial attempt.
+// // Time complexity is O(n^2)
+//
+// using namespace std;
+// void rotate(int arr[], int d, int n) {
+//     int el;
+//     for(int i = 0; i < d; i++) {
+//         el = arr[0];
+        
+//         for(int i = 0; i < n-1; i++) {
+//             arr[i] = arr[i+1];
+//         }
+        
+//         arr[n-1] = el;
+//     }
+//     printArray(arr, n);     
+// }
 
 int main()
 {
