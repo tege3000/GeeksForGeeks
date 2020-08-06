350f69bb3a6b18d6736269082f91fb66cec118fb
Tito <teetoegeonu@gmail.com>
2020-08-06 16:42:53 +0100

    ADD - second attempt at arranhging given numbers in an array to form the biggest number

--- geeksforgeeks-solved/Arrays/ArrayRearrangement/15-/arrange.cpp
+++ geeksforgeeks-solved/Arrays/ArrayRearrangement/15-/arrange.cpp
@@ -10,82 +10,102 @@
  *
  */
 
+
+
 #include <iostream>
-#include <vector>
-
 using namespace std;
+// Second Attempt
+//
+int compareValues(string x, string y) {
+    string res1 = x + y;
+    string res2 = y + x;
 
-void printArr(string arr[], int n) {
+    if(stoi(res1) > stoi(res2)) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+// code here is much smaller
+void arrange(string arr[], int n) {
+    sort(arr+0, arr+n, compareValues);
+    
     for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+        cout << arr[i];
     }
     cout << "\n";
 }
 
-string compareValues(string x, string y) {
-    string res1 = x + y;
-    string res2 = y + x;
-    
-    if(stoi(res1) > stoi(res2)) {
-        return res1;
-    }
-    else {
-        return res2;
-    }
-}
 
-void arrange(string arr[], int n) {
-    vector<pair<char, int>> v1;
-    
-    for(int i = 0; i < n; i++) {
-        pair <char, int> inner;
-        
-        inner = make_pair(arr[i][0], i);
-        v1.push_back(inner);
-    }
-    
-    sort(v1.begin(), v1.end());
-    reverse(v1.begin(), v1.end());
-    
-    for(int i = 0; i < v1.size(); i++) {
-        cout << v1[i].first << " " << v1[i].second << endl;
-    }
-    
-    string ans = "";
-    string s1, s2;
-    for(int i = 0; i < v1.size(); i++) {
-        if(v1[i].first == v1[i+1].first) {
-            for(int k = 0; k < n; k++) {
-                if(v1[i].second == k) {
-                    s1 = arr[k];
-                }
-                
-                if(v1[i+1].second == k) {
-                    s2 = arr[k];
-                }
-            }
-            ans = ans + compareValues(s1, s2);
-            i++;
-            
-        }
-        else {
-            for(int j = 0; j < n; j++) {
-                if(v1[i].second == j) {
-                    ans = ans + arr[j];
-                }
-            }
-            
-        }
-        
-    }
-    
-    cout << ans << endl;
-}
+// Initial Attempt
+//
+//string compareValues(string x, string y) {
+//    string res1 = x + y;
+//    string res2 = y + x;
+//
+//    if(stoi(res1) > stoi(res2)) {
+//        return res1;
+//    }
+//    else {
+//        return res2;
+//    }
+//}
+//
+//void arrange(string arr[], int n) {
+//    vector<pair<char, int>> v1;
+//
+//    for(int i = 0; i < n; i++) {
+//        pair <char, int> inner;
+//
+//        inner = make_pair(arr[i][0], i);
+//        v1.push_back(inner);
+//    }
+//
+//    sort(v1.begin(), v1.end());
+//    reverse(v1.begin(), v1.end());
+//
+//    for(int i = 0; i < v1.size(); i++) {
+//        cout << v1[i].first << " " << v1[i].second << endl;
+//    }
+//
+//    string ans = "";
+//    string s1, s2;
+//    for(int i = 0; i < v1.size(); i++) {
+//        if(v1[i].first == v1[i+1].first) {
+//            for(int k = 0; k < n; k++) {
+//                if(v1[i].second == k) {
+//                    s1 = arr[k];
+//                }
+//
+//                if(v1[i+1].second == k) {
+//                    s2 = arr[k];
+//                }
+//            }
+//            ans = ans + compareValues(s1, s2);
+//            i++;
+//
+//        }
+//        else {
+//            for(int j = 0; j < n; j++) {
+//                if(v1[i].second == j) {
+//                    ans = ans + arr[j];
+//                }
+//            }
+//
+//        }
+//
+//    }
+//
+//    cout << ans << endl;
+//}
+
 int main() {
-    //string arr[] = {"54", "546", "548", "60"};
-    string arr[] = {"1", "34", "3", "98", "9", "76", "45", "4"};
+    string arr[] = {"54", "546", "548", "60"};
+    //string arr[] = {"1", "34", "3", "98", "9", "76", "45", "4"};
     
     int n = sizeof(arr)/sizeof(arr[0]);
     
-    arrange(arr,n);
+    arrange(arr, n);
 }
