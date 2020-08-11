4fdc31ff78f85dccd051f31abe375c4f904c85ce
Tito <teetoegeonu@gmail.com>
2020-08-11 12:57:45 +0100

    ADD - Implemented algorithm to find the kth smallest element in an unsorted array

---
+++ geeksforgeeks-solved/Arrays/Order Statistics/1- kth-smallestlargest-element-unsorted-array/findKthSmallestEl.cpp
@@ -0,0 +1,51 @@
+/*
+ * Topic: K’th Smallest/Largest Element in Unsorted
+ * Array | Set 1
+ *
+ * Description: Given an array and a number k where
+ * k is smaller than size of array, we need to find
+ * the k’th smallest element in the given array. It
+ * is given that ll array elements are distinct.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// first implementation
+// O(n) time
+int findKthSmallestEl(int arr[], int n, int k) {
+    int min;
+    int max = *max_element(arr, arr+n);
+    for(int i = 0; i < k; i++) {
+        min = *min_element(arr, arr+n);
+        if(i < k-1) {
+            auto it = find(arr, arr+n, min);
+            int index = distance(arr, it);
+            if(it != arr+n) {
+                arr[index] = max;
+            }
+        }
+    }
+    
+    return min;
+}
+
+void printArr(int arr[], int n) {
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int arr[] =  {7, 10, 4, 3, 20, 15};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 4;
+    
+    int ans = findKthSmallestEl(arr, n, k);
+    
+    cout << ans << endl;
+    //printArr(arr, n);
+}
+
