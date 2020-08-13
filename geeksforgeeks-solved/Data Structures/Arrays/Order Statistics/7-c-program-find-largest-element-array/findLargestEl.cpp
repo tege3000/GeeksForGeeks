1ad788e90c69427f6fa5b7bc3588aa9b09ca5adc
Tito <teetoegeonu@gmail.com>
2020-08-13 20:39:17 +0100

    UPD - added second approach to finding the max element in an array, basically used stl function

--- geeksforgeeks-solved/Data Structures/Arrays/Order Statistics/7-c-program-find-largest-element-array/findLargestEl.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Order Statistics/7-c-program-find-largest-element-array/findLargestEl.cpp
@@ -9,11 +9,18 @@
 
 using namespace std;
 
+// second approach
 int findLargestEl(int arr[], int n) {
-    sort(arr, arr+n);
-    
-    return arr[n-1];
+    return *max_element(arr, arr+n);
 }
+
+// initial approach
+//
+//int findLargestEl(int arr[], int n) {
+//    sort(arr, arr+n);
+//
+//    return arr[n-1];
+//}
 
 void printArr(int arr[], int n) {
     for(int i = 0; i < n; i++) {
