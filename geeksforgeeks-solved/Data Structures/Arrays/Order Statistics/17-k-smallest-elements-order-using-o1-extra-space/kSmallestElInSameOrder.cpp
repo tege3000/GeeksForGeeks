330a99dd2956a3755615f9e3271d86dec4a01788
Tito <teetoegeonu@gmail.com>
2020-08-19 13:24:12 +0100

    UPD - Added second implementation for algorithm to find the k smallest element in same order

--- geeksforgeeks-solved/Data Structures/Arrays/Order Statistics/17-k-smallest-elements-order-using-o1-extra-space/kSmallestElInSameOrder.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Order Statistics/17-k-smallest-elements-order-using-o1-extra-space/kSmallestElInSameOrder.cpp
@@ -11,39 +11,67 @@
 
 using namespace std;
 
+// second implementation; after looking at geeksforgeeks solution
+// O(n^2) time
 void kSmallestElInSameOrder(int arr[], int n, int k) {
-    int size = n-k;
-    for(int i = 0; i < size; i++) {
-        auto maxEl = max_element(arr, arr+n);
-        int pos = distance(arr, maxEl);
+    int pos = 0;
+    for(int i = k; i < n; i++) {
+        auto maxEl = max_element(arr, arr+k);
+        pos = distance(arr, maxEl);
         
-        // basically this (technically) deletes the element from the array
-        // This happens by performing arr[i] = arr[i+1] from the index to delete
-        // then copying all subsequent elements from the next index to the
-        // prev index.
-        // Then finally reducing the size of the array by 1
-        for(int i = pos; i < n-1; i++) {
-            arr[i] = arr[i+1];
+        if(*maxEl > arr[i]) {
+            for(int j = pos; j < k-1; j++) {
+                arr[j] = arr[j+1];
+            }
+            
+            arr[k-1] = arr[i];
         }
-        n--;
+        
     }
     
-    for(int i = 0; i < n; i++) {
-        if(arr[i] != -1) {
-            cout << arr[i] << " ";
-        }
+    for(int i = 0; i < k; i++) {
+        cout << arr[i] << " ";
     }
     cout << "\n";
-
 }
 
+//// initial implementation
+//// O(n^2) time
+//// NOTE: Normally, this approach makes more sense to me
+//
+//void kSmallestElInSameOrder(int arr[], int n, int k) {
+//    int size = n-k;
+//    for(int i = 0; i < size; i++) {
+//        auto maxEl = max_element(arr, arr+n);
+//        int pos = distance(arr, maxEl);
+//
+//        // basically this (technically) deletes the element from the array
+//        // This happens by performing arr[i] = arr[i+1] from the index to delete
+//        // then copying all subsequent elements from the next index to the
+//        // prev index.
+//        // Then finally reducing the size of the array by 1
+//        for(int i = pos; i < n-1; i++) {
+//            arr[i] = arr[i+1];
+//        }
+//        n--;
+//    }
+//
+//    for(int i = 0; i < n; i++) {
+//        if(arr[i] != -1) {
+//            cout << arr[i] << " ";
+//        }
+//    }
+//    cout << "\n";
+//
+//}
+
 int main() {
-    int arr[] =  {4, 2, 6, 1, 5};
+    //int arr[] =  {4, 2, 6, 1, 5};
     //int arr[] = {4, 12, 16, 21, 25};
-    //int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+    int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
     int n = sizeof(arr)/sizeof(arr[0]);
-    //int k = 5;
-    int k = 3;
+    int k = 5;
+    //int k = 3;
     
     kSmallestElInSameOrder(arr, n, k);
     return 0;
