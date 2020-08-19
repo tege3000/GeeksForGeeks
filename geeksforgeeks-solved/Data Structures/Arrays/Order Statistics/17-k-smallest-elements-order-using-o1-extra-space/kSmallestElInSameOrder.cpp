7b1b61d864e4a8150d8087250b380f2fd25b2ba9
Tito <teetoegeonu@gmail.com>
2020-08-19 12:58:49 +0100

    ADD - Implemented algorithm to find the k smallest elemebts in the same order as in original array, using O(1) extra space

---
+++ geeksforgeeks-solved/Data Structures/Arrays/Order Statistics/17-k-smallest-elements-order-using-o1-extra-space/kSmallestElInSameOrder.cpp
@@ -0,0 +1,50 @@
+/*
+ * Topic: k smallest elements in same order using O(1)
+ * extra space
+ *
+ * Description: You are given an array of n-elements you
+ * have to find k smallest elements from the array but
+ * they must be in the same order as they are in given
+ * array and we are allowed to use only O(1) extra space.
+ */
+#include <iostream>
+
+using namespace std;
+
+void kSmallestElInSameOrder(int arr[], int n, int k) {
+    int size = n-k;
+    for(int i = 0; i < size; i++) {
+        auto maxEl = max_element(arr, arr+n);
+        int pos = distance(arr, maxEl);
+        
+        // basically this (technically) deletes the element from the array
+        // This happens by performing arr[i] = arr[i+1] from the index to delete
+        // then copying all subsequent elements from the next index to the
+        // prev index.
+        // Then finally reducing the size of the array by 1
+        for(int i = pos; i < n-1; i++) {
+            arr[i] = arr[i+1];
+        }
+        n--;
+    }
+    
+    for(int i = 0; i < n; i++) {
+        if(arr[i] != -1) {
+            cout << arr[i] << " ";
+        }
+    }
+    cout << "\n";
+
+}
+
+int main() {
+    int arr[] =  {4, 2, 6, 1, 5};
+    //int arr[] = {4, 12, 16, 21, 25};
+    //int arr[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+    //int k = 5;
+    int k = 3;
+    
+    kSmallestElInSameOrder(arr, n, k);
+    return 0;
+}
