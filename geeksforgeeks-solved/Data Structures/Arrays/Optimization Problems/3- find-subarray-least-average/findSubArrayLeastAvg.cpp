d355dd32876f288df6dd1e825acffadfe2216f21
Tito <teetoegeonu@gmail.com>
2020-09-02 13:59:27 +0100

    UPD - optimized algorithm.

--- geeksforgeeks-solved/Data Structures/Arrays/Optimization Problems/3- find-subarray-least-average/findSubArrayLeastAvg.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Optimization Problems/3- find-subarray-least-average/findSubArrayLeastAvg.cpp
@@ -10,30 +10,59 @@
 
 using namespace std;
 
+// second apprach
+// much better time complexity
+// O(n)
 void findSubArrayLeastAvg(int arr[], int n, int k) {
-    int avg = 1000000000; // arbitrary large value
     int l, r;
- 
-    for(int i = 0; i < n-k; i++) {
-        int sum = 0;
-        for(int j = i; j < i+k; j++) {
-            sum += arr[j];
-        }
-        if(sum/k < avg) {
-            avg = sum/k;
-            l = i;
-            r =  r = i+k-1;
-        }
+    int avg = 0;
+    int count = 0;
+    int sum = 0;
+    for(int i = 0; i < k; i++) {
+        sum += arr[i];
     }
     
+    int min = sum;
+    for(int i = k; i < n; i++) {
+        sum += arr[i] - arr[k-i];
+        
+        if(sum < min) {
+            avg = sum/k;
+            min = sum;
+            l = i-k+1;
+            r = i;
+        }
+        
+    }
     cout << "Subarray between indexes " << l << " and " << r << " has minimum avegrage\n";
 }
+
+
+// Initial approach  O(nk)
+//void findSubArrayLeastAvg(int arr[], int n, int k) {
+//    int avg = 1000000000; // arbitrary large value
+//    int l, r;
+//
+//    for(int i = 0; i < n-k; i++) {
+//        int sum = 0;
+//        for(int j = i; j < i+k; j++) {
+//            sum += arr[j];
+//        }
+//        if(sum/k < avg) {
+//            avg = sum/k;
+//            l = i;
+//            r =  r = i+k-1;
+//        }
+//    }
+//
+//    cout << "Subarray between indexes " << l << " and " << r << " has minimum avegrage\n";
+//}
 
 int main() {
     
     int arr[] =  {3, 7, 5, 20, -10, 0, 12};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int k = 2;
+    int k = 3;
     
     findSubArrayLeastAvg(arr,n, k);
 }
