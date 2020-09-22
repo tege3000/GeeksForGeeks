e97d777624a94b5f67fe1ebfb833903cc05ca5fc
Tito <teetoegeonu@gmail.com>
2020-09-22 15:28:50 +0100

    UPD - Improved time complexity of algorithm

--- geeksforgeeks-solved/Data Structures/Arrays/Sorting/21- count-pairs-difference-equal-k/countDistinctPairs.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Sorting/21- count-pairs-difference-equal-k/countDistinctPairs.cpp
@@ -8,18 +8,37 @@
 #include <iostream>
 using namespace std;
 
+// Better approach
+// Works in O(nlogn) time, because its sorts and then uses binary search
 int countDistinctPairs(int arr[], int n, int k) {
     int count = 0;
+    
+    sort(arr, arr+n);
+    
     for(int i = 0; i < n; i++) {
-        for(int j = i+1; j < n; j++) {
-            if(abs(arr[i] - arr[j]) == k) {
-                count++;
-            }
+        if(binary_search(arr+i, arr+n, arr[i]+k)) {
+            count++;
         }
     }
     
     return count;
 }
+
+
+//// Initial Approcach
+//// Works in O(n^2) time
+//int countDistinctPairs(int arr[], int n, int k) {
+//    int count = 0;
+//    for(int i = 0; i < n; i++) {
+//        for(int j = i+1; j < n; j++) {
+//            if(abs(arr[i] - arr[j]) == k) {
+//                count++;
+//            }
+//        }
+//    }
+//
+//    return count;
+//}
 
 int main() {
     int arr[] = {8, 12, 16, 4, 0, 20}, k = 4;
