059c5c5f537ce16684be229bd388a7aaa02522ba
Tito <teetoegeonu@gmail.com>
2020-09-23 12:21:15 +0100

    UPD - Implemented new algorithm for finding pair whose sum is equal to sum

--- geeksforgeeks-solved/Data Structures/Arrays/Searching/3- given-an-array-a-and-a-number-x-check-for-pair-in-a-with-sum-as-x/findPair.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Searching/3- given-an-array-a-and-a-number-x-check-for-pair-in-a-with-sum-as-x/findPair.cpp
@@ -9,24 +9,52 @@
 #include <iostream>
 using namespace std;
 
+// Second Approach - Using two pointer method
+// Works in O(nlogn) time
 void findPair(int arr[], int n, int sum) {
+    int left = 0, right = n-1;
     int found = 0;
+    sort(arr, arr+n);
     for(int i = 0; i < n; i++) {
-        for(int j = i+1; j < n; j++) {
-            if(arr[i] + arr[j] == sum) {
-                cout << arr[i] << ", " << arr[j] << endl;
-                found = 1;
-            }
+        if(sum > arr[left] + arr[right]) {
+            left++;
+        }
+        else if(sum < arr[left] + arr[right]) {
+            right--;
+        }
+        else {
+            cout << arr[left] << ", " << arr[right] << endl;
+            found = 1;
+            break;
         }
     }
+    
     if(found == 0) {
         cout << -1 << endl;
     }
+    
 }
 
+//// First Approach,
+//// Works in O(n^2) time
+//void findPair(int arr[], int n, int sum) {
+//    int found = 0;
+//    for(int i = 0; i < n; i++) {
+//        for(int j = i+1; j < n; j++) {
+//            if(arr[i] + arr[j] == sum) {
+//                cout << arr[i] << ", " << arr[j] << endl;
+//                found = 1;
+//            }
+//        }
+//    }
+//    if(found == 0) {
+//        cout << -1 << endl;
+//    }
+//}
+
 int main() {
-    int arr[] = {1, -2, 1, 0, 5};
-    int sum = 0;
+    int arr[] = {0, -1, 2, -3, 1};
+    int sum = -2;
     int n = sizeof(arr)/sizeof(arr[0]);
     
     findPair(arr, n, sum);
