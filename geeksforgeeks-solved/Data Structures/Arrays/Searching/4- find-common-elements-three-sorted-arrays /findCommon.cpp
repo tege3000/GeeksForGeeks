c092fb6c9012731e3c946e919dde0a269278deb1
Tito <teetoegeonu@gmail.com>
2020-09-25 14:07:31 +0100

    UPD - Added a much better algorithm for finding common elements in three sorted array.

--- geeksforgeeks-solved/Data Structures/Arrays/Searching/4- find-common-elements-three-sorted-arrays /findCommon.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Searching/4- find-common-elements-three-sorted-arrays /findCommon.cpp
@@ -7,39 +7,67 @@
 #include <iostream>
 using namespace std;
 
+// Second Approach - Much better
+// Works in O(n) time
 void findCommon(int ar1[], int ar2[], int ar3[], int n1, int n2, int n3) {
-    if(n1 <= n2 & n1 <= n3) {
-        for(int i = 0; i < n1; i++) {
-            if((find(ar2, ar2+n2, ar1[i]) != ar2+n2) && (find(ar3, ar3+n3, ar1[i]) != ar3+n3)) {
-                cout << ar1[i] << " ";
-            }
-        }
-        cout << "\n";
-    }
-    else if(n2 <= n1 && n2 <= n3) {
-        for(int i = 0; i < n2; i++) {
-            if((find(ar1, ar1+n1, ar2[i]) != ar1+n1) && (find(ar3, ar3+n3, ar2[i]) != ar3+n3)) {
-                cout << ar2[i] << " ";
-            }
-        }
-        cout << "\n";
-    }
-    else {
-        for(int i = 0; i < n3; i++) {
-            if((find(ar1, ar1+n1, ar3[i]) != ar1+n1) && (find(ar2, ar2+n2, ar3[i]) != ar2+n2)) {
-                cout << ar3[i] << " ";
-            }
-        }
-        cout << "\n";
-    }
+    int i = 0, j = 0, k = 0;
     
+    while(i < n1 && j < n2 && k < n3) {
+        if(ar1[i] == ar2[j] && ar2[j] == ar3[k]) {
+            cout << ar1[i] << " ";
+            i++;
+            j++;
+            k++;
+        }
+        else if(ar1[i] < ar2[j]) {
+            i++;
+        }
+        else if(ar2[j] < ar3[k]) {
+            j++;
+        }
+        else {
+            k++;
+        }
+        
+    }
 }
+
+
+//// Initial Approach
+//// Works in O(n^2) time
+//void findCommon(int ar1[], int ar2[], int ar3[], int n1, int n2, int n3) {
+//    if(n1 <= n2 & n1 <= n3) {
+//        for(int i = 0; i < n1; i++) {
+//            if((find(ar2, ar2+n2, ar1[i]) != ar2+n2) && (find(ar3, ar3+n3, ar1[i]) != ar3+n3)) {
+//                cout << ar1[i] << " ";
+//            }
+//        }
+//        cout << "\n";
+//    }
+//    else if(n2 <= n1 && n2 <= n3) {
+//        for(int i = 0; i < n2; i++) {
+//            if((find(ar1, ar1+n1, ar2[i]) != ar1+n1) && (find(ar3, ar3+n3, ar2[i]) != ar3+n3)) {
+//                cout << ar2[i] << " ";
+//            }
+//        }
+//        cout << "\n";
+//    }
+//    else {
+//        for(int i = 0; i < n3; i++) {
+//            if((find(ar1, ar1+n1, ar3[i]) != ar1+n1) && (find(ar2, ar2+n2, ar3[i]) != ar2+n2)) {
+//                cout << ar3[i] << " ";
+//            }
+//        }
+//        cout << "\n";
+//    }
+//
+//}
 
 int main()
 {
-    int ar1[] = {1, 5, 5};
-    int ar2[] = {3, 4, 5, 5, 10};
-    int ar3[] = {5, 5, 10, 20};
+    int ar1[] = {1, 5, 10, 20, 40, 80};
+    int ar2[] = {6, 7, 20, 80, 100};
+    int ar3[] = {3, 4, 15, 20, 30, 70, 80, 120};
     int n1 = sizeof(ar1)/sizeof(ar1[0]);
     int n2 = sizeof(ar2)/sizeof(ar2[0]);
     int n3 = sizeof(ar3)/sizeof(ar3[0]);
