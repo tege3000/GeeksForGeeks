1af2a1cba2a5e3f61404c00d3b94aec021df3138
Tito <teetoegeonu@gmail.com>
2020-09-29 14:40:05 +0100

    UPD - Added better approach

--- geeksforgeeks-solved/Data Structures/Arrays/Searching/12-/printLeaders.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Searching/12-/printLeaders.cpp
@@ -9,18 +9,37 @@
  * leaders are 17, 5 and 2.
  */
 #include <iostream>
+#include <limits.h>
 using namespace std;
 
-// Initial approach
-// Works in O(n^2) time
+// Better approach
+// Works in O(n) time
+//
+// How it works:
+// Scan all the elements from right to left in an array
+// and keep track of maximum till now. When maximum changes
+//  its value, print it.
 void printLeaders(int arr[], int n) {
-    for(int i = 0; i < n; i++) {
-        if(arr[i] > *max_element(arr+i+1, arr+n)) {
+    int maxEl = INT_MIN;
+    for(int i = n-1; i >= 0; i--) {
+        if(arr[i] > maxEl) {
+            maxEl = arr[i];
             cout << arr[i] << " ";
         }
     }
     cout << "\n";
 }
+
+//// Initial approach
+//// Works in O(n^2) time
+//void printLeaders(int arr[], int n) {
+//    for(int i = 0; i < n; i++) {
+//        if(arr[i] > *max_element(arr+i+1, arr+n)) {
+//            cout << arr[i] << " ";
+//        }
+//    }
+//    cout << "\n";
+//}
 
 int main() {
     int arr[] = {16, 17, 4, 3, 5, 2};
