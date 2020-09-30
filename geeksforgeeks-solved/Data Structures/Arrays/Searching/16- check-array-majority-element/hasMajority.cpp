da00a073366a6872c1dd9b29323d8828d811d8bb
Tito <teetoegeonu@gmail.com>
2020-09-30 14:30:55 +0100

    ADD - Implemented better approach for checking if an array has majority element

--- geeksforgeeks-solved/Data Structures/Arrays/Searching/16- check-array-majority-element/hasMajority.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Searching/16- check-array-majority-element/hasMajority.cpp
@@ -5,35 +5,53 @@
  * the input array contains a majority element or not.
  */
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
-
-// Simple - Initial approach
-// Works in O(nlogn) time
+// Better approach - uses hashing
+// Works in O(n) time
 int hasMajority(int arr[], int n) {
-    int count = 1;
-    sort(arr, arr+n);
-
+    
+    unordered_map<int, int> mp;
     for(int i = 0; i < n; i++) {
-        if(arr[i] == arr[i+1]) {
-            count++;
-        }
-        else {
-            if(count > n/2) {
-                return true;
-            }
-            count = 1;
+        mp[arr[i]]++;
+    }
+    
+    for(auto i: mp) {
+        if(i.second > n/2) {
+            return true;
         }
     }
     
     return false;
-    
 }
+
+//// Simple - Initial approach
+//// Works in O(nlogn) time
+//int hasMajority(int arr[], int n) {
+//    int count = 1;
+//    sort(arr, arr+n);
+//
+//    for(int i = 0; i < n; i++) {
+//        if(arr[i] == arr[i+1]) {
+//            count++;
+//        }
+//        else {
+//            if(count > n/2) {
+//                return true;
+//            }
+//            count = 1;
+//        }
+//    }
+//
+//    return false;
+//
+//}
 
 
 int main() {
-    int arr[] = {1, 8, 9, 2, 5};
+    int arr[] = {2, 3, 9, 2, 2};
     int arr_size = sizeof(arr) / sizeof(arr[0]);
     
     if(hasMajority(arr, arr_size)) {
