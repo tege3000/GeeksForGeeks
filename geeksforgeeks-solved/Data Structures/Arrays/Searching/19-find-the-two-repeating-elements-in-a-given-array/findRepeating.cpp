f91c1eb73ad5c5e4f3a14175abd98325b4ae5f05
Tito <teetoegeonu@gmail.com>
2020-10-01 12:23:06 +0100

    ADD - Initial implementation for finding repeated elements in an array

---
+++ geeksforgeeks-solved/Data Structures/Arrays/Searching/19-find-the-two-repeating-elements-in-a-given-array/findRepeating.cpp
@@ -0,0 +1,34 @@
+/*
+ * Topic: Find the two repeating elements in a given array
+ *
+ * Description: You are given an array of n+2 elements.
+ * All elements of the array are in range 1 to n. And all
+ * elements occur once except two numbers which occur twice.
+ * Find the two repeating numbers.
+ * For example, array = {4, 2, 4, 5, 2, 3, 1} and n = 5
+ * The above array has n + 2 = 7 elements with all elements
+ * occurring once except 2 and 4 which occur twice. So the
+ * output should be 4 2.
+ */
+#include <iostream>
+using namespace std;
+
+void findRepeating(int arr[], int n) {
+    sort(arr, arr+n);
+    int count = 1;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == arr[i+1]) {
+            cout << arr[i] << " ";
+        }
+    }
+    
+    cout << "\n";
+}
+
+int main() {
+    int arr[] = {4, 2, 4, 5, 2, 3, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    findRepeating(arr, n);
+    return 0;
+}
