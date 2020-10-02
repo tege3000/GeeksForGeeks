cb5a876100ddfe18293412c9b0543d22106f79e6
Tito <teetoegeonu@gmail.com>
2020-10-02 13:30:48 +0100

    ADD - implemented algorithm for finding subarray with given sum

--- geeksforgeeks-solved/Data Structures/Arrays/Searching/21- find-subarray-with-given-sum/findSubArray.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Searching/21- find-subarray-with-given-sum/findSubArray.cpp
@@ -3,22 +3,32 @@
  *
  * Description: Given an unsorted array of nonnegative integers, find
  * a continuous subarray which adds to a given number.
- 
  */
 #include <iostream>
+#include <numeric>
 using namespace std;
 
-int findSubArray(int arr[], int n, int sum) {
-    return -1;
+bool findSubArray(int arr[], int n, int sum) {
+    
+    for(int i = 0; i < n; i++) {
+        int sumOfEls = 0;
+        for(int j = i; j < n; j++) {
+            sumOfEls = accumulate(arr+i, arr+j+1, 0);
+            if(sumOfEls == sum) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 int main() {
-    int arr[] = {-10, -1, 0, 3, 10, 11, 30, 50, 100};
+    int arr[] = {1, 4, 0, 0, 3, 10, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int sum = 33;
+    int sum = 7;
     int res = findSubArray(arr, n, sum);
-    if(res != -1) {
-        cout << "Output: " << res << "\n";
+    if(res) {
+        cout << "Sum found" << "\n";
     }
     else {
         cout << "There is no subarray with " << sum << " sum\n";
