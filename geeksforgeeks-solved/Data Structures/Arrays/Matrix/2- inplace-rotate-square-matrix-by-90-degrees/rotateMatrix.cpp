13594c199c1340ffe89f51f7e1843d8bf5c789bf
Tito <teetoegeonu@gmail.com>
2020-10-06 09:13:18 +0100

    UPD - Finally finished implementing algorithm to rotate a matrix in place. Time complexity might not be the best, nevertheless this one was interesting

--- geeksforgeeks-solved/Data Structures/Arrays/Matrix/2- inplace-rotate-square-matrix-by-90-degrees/rotateMatrix.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Matrix/2- inplace-rotate-square-matrix-by-90-degrees/rotateMatrix.cpp
@@ -7,8 +7,8 @@
  * anti-clockwise direction without using any extra space.
  */
 #include <iostream>
-#include <vector>
-#include <set>
+#include <iomanip>
+
 #define N 4
 
 using namespace std;
@@ -17,43 +17,48 @@ void rotateMatrix(int arr[N][N]) {
     int prev = 0, last = 0;
     int rCounter = 0;
     int colCounter = 0;
+    int len  = 0;
     
-    // Progress Made: Successfully rotated outer most cycle of matrix
-    // TODO: Make algorithm iterative for all cycles, instead of just 1
-    for(int start = 0; start < N-1; start++) {
-        prev = arr[start][N-1];
-        for(int i = 0; i < 4; i++) {
-            if(i == 0) {
-                rCounter = 0;
-                colCounter = start;
-                last = arr[rCounter][colCounter];
-                arr[rCounter][colCounter] = prev;
-                prev = last;
-            }
-            else if(i == 1) {
-                rCounter = (N-1) - start;
-                colCounter = 0;
-                last = arr[rCounter][colCounter];
-                arr[rCounter][colCounter] = prev;
-                prev = last;
-            }
-            else if(i == 2) {
-                rCounter = N-1;
-                colCounter = (N-1)  - start;
-                last = arr[rCounter][colCounter];
-                arr[rCounter][colCounter] = prev;
-                prev = last;
-            }
-            else {
-                rCounter = start;
-                colCounter = N-1;
-                last = arr[rCounter][colCounter];
-                arr[rCounter][colCounter] = prev;
-                prev = last;
+    for(int cycle = 0; cycle < (N/2); cycle++) {
+        len = (N-1) - cycle;
+        for(int start = cycle; start < len; start++) {
+            
+            prev = arr[start][len];
+            for(int i = 0; i < 4; i++) {
+                if(i == 0) {
+                    rCounter = cycle;
+                    colCounter = start;
+                    last = arr[rCounter][colCounter];
+                    arr[rCounter][colCounter] = prev;
+                    prev = last;
+                }
+                else if(i == 1) {
+                    rCounter = len - start + cycle;
+                    colCounter = cycle;
+                    last = arr[rCounter][colCounter];
+                    arr[rCounter][colCounter] = prev;
+                    prev = last;
+                }
+                else if(i == 2) {
+                    rCounter = len;
+                    colCounter = len - start + cycle;
+                    last = arr[rCounter][colCounter];
+                    arr[rCounter][colCounter] = prev;
+                    prev = last;
+                }
+                else {
+                    rCounter = start;
+                    colCounter = len;
+                    last = arr[rCounter][colCounter];
+                    arr[rCounter][colCounter] = prev;
+                    prev = last;
+                }
             }
         }
     }
-
+    
+    
+    
 }
 
 void displayMatrix(int arr[N][N]) {
@@ -67,7 +72,7 @@ void displayMatrix(int arr[N][N]) {
 
 int main()
 {
-//    // Test Case 1
+    //    // Test Case 1
     int mat[N][N] = {
         { 1, 2, 3, 4 },
         { 5, 6, 7, 8 },
@@ -75,21 +80,19 @@ int main()
         { 13, 14, 15, 16 }
     };
     
-//     Tese Case 2
-//     int mat[N][N] = {
-//     {1, 2, 3},
-//     {4, 5, 6},
-//     {7, 8, 9}
-//     };
+////         Tese Case 2
+//         int mat[N][N] = {
+//         {1, 2, 3},
+//         {4, 5, 6},
+//         {7, 8, 9}
+//         };
     
     
     // Tese Case 3
-    /*int mat[N][N] = {
-     {1, 2},
-     {4, 5}
-     };*/
-    
-    // displayMatrix(mat);
+//    int mat[N][N] = {
+//     {1, 2},
+//     {4, 5}
+//     };
     
     rotateMatrix(mat);
     
