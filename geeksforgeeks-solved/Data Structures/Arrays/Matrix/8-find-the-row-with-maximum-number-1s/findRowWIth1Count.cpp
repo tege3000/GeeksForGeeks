aef86361a68de613f1d014999a01e2317c01f939
Tito <teetoegeonu@gmail.com>
2020-10-08 09:51:38 +0100

    UPD - Added second approach

--- geeksforgeeks-solved/Data Structures/Arrays/Matrix/8-find-the-row-with-maximum-number-1s/findRowWIth1Count.cpp
+++ geeksforgeeks-solved/Data Structures/Arrays/Matrix/8-find-the-row-with-maximum-number-1s/findRowWIth1Count.cpp
@@ -10,20 +10,65 @@
 #define N 4
 using namespace std;
 
-int findRowWithMaxNumOf1s(int arr[N][N]) {
-    int maxCount = 0;
-    int currCount = 0;
-    int row = 0;
-    for(int i = 0; i < N; i++) {
-        currCount = count(arr[i], arr[i]+N, 1);
-        if(currCount > maxCount) {
-            maxCount = currCount;
-            row = i;
+
+// Modified version of binary search to find index of first
+// occurrence of an element in an array
+int binSearch(int arr[N], int start, int end, int val) {
+    int result = -1;
+    while(start <= end) {
+        int mid = (start+end)/2;
+        if(arr[mid] == val) {
+            result = mid;
+            end = mid-1;
+        }
+        else if(arr[mid] > val) {
+            start = mid+1;
+        }
+        else {
+            end = mid-1;
         }
     }
     
-    return row;
+    return result;
 }
+
+// Second Implementation
+// Works in O(nlogn) time
+int findRowWithMaxNumOf1s(int arr[N][N]) {
+    int countOf1s = 0;
+    int maxCount = 0;
+    int ans = 0;
+    for(int i = 0; i < N; i++) {
+        int index = binSearch(arr[i], 0, N-1, 1);
+        if(index != -1) {
+            countOf1s = N - index;
+            if(countOf1s > maxCount) {
+                maxCount = countOf1s;
+                ans = i;
+            }
+            
+        }
+    }
+    
+    return ans;
+}
+
+// Initial Implementation
+// Works in O(n^2) time
+//int findRowWithMaxNumOf1s(int arr[N][N]) {
+//    int maxCount = 0;
+//    int currCount = 0;
+//    int row = 0;
+//    for(int i = 0; i < N; i++) {
+//        currCount = count(arr[i], arr[i]+N, 1);
+//        if(currCount > maxCount) {
+//            maxCount = currCount;
+//            row = i;
+//        }
+//    }
+//
+//    return row;
+//}
 
 
 int main() {
