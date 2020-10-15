011bc3478437c33b4985f7ef133979087c52e293
Tito <teetoegeonu@gmail.com>
2020-10-15 09:54:41 +0100

    UPD - refactored code. Implemented a much better algorithm that covers a larger scope

--- geeksforgeeks-solved/Data-Structures/Strings/Basics/5- removing-punctuations-given-string/removePunctuations.cpp
+++ geeksforgeeks-solved/Data-Structures/Strings/Basics/5- removing-punctuations-given-string/removePunctuations.cpp
@@ -6,33 +6,34 @@
  * Given a string, remove the punctuation from the string
  * if the given character is a punctuation character as
  * classified by the current C locale. The default C locale
- * classifies these characters as punctuation:
+ * classifies these (any many more) characters as punctuation:
  * !"#$%&'()*+,-./:;?@[\]^_`{|}~
  */
 #include <iostream>
 using namespace std;
 
-// Initial approach
+// Better Approach -
+// I just learnt that c++ stl has
+// a function to check if a character is classified
+// as a punctuation under the C locale
 string removePunctuations(string s) {
-    // NOTE: HAD TO ADD ESCAPE CHARACTERS
-    // FOR " AND \ IN THE PUNCTUATIONS STRING
-    string punctuations = "!\"#$%&'()*+,-./:;?@[\\]^_`{|}~";
     
     for(int i = 0; i < s.size(); i++) {
-        if(punctuations.find(s[i]) != -1) {
+        if(ispunct(s[i])) {
             s.erase(s.begin()+i);
             i--;
         }
     }
+           
     return s;
-    
+           
 }
 
 int main() {
-    string s = "%welcome' to @geeksforgeeks";
-    cout << removePunctuations(s) << "\n";
-    
-    string s2 = "Hello!!!, he said ---and went.";
-    cout << removePunctuations(s2) << "\n";
-    return 0;
+   string s = "Welcome???@@##$ to#$% Geeks%$^for$%^&Geeks";
+   cout << removePunctuations(s) << "\n";
+   
+   string s2 = "Hello!!!, he said ---and went.";
+   cout << removePunctuations(s2) << "\n";
+   return 0;
 }
