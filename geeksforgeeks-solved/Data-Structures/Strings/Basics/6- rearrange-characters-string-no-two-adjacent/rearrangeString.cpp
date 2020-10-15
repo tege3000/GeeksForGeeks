1c0831eb842a515f4cd41a14de44ee6ad8f67661
Tito <teetoegeonu@gmail.com>
2020-10-15 10:06:47 +0100

    ADD - Setup boilerplate code for next task

---
+++ geeksforgeeks-solved/Data-Structures/Strings/Basics/6- rearrange-characters-string-no-two-adjacent/rearrangeString.cpp
@@ -0,0 +1,26 @@
+/*
+ * Topic:
+ * Rearrange characters in a string such that no two adjacent are same
+ *
+ * Description:
+ * Given a string with repeated characters, the task is to rearrange
+ * characters in a string so that no two adjacent characters are same.
+ * Note : It may be assumed that the string has only lowercase English alphabets.
+ */
+#include <iostream>
+using namespace std;
+
+string rearrangeString(string s) {
+    
+    // TODO: figure this out!, seems like an interesting one
+    return s;
+           
+}
+
+int main() {
+    string s = "aaabc";
+    cout << rearrangeString(s) << "\n";
+    
+   
+    return 0;
+}
