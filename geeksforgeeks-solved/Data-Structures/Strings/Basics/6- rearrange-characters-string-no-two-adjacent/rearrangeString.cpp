8ebdecca5e7dc738530510e657b472adc53d2fa5
Tito <teetoegeonu@gmail.com>
2020-10-16 09:41:55 +0100

    UPD - Implemented algorithm to rearrange characters in a string st no two adjacent els are the same

--- geeksforgeeks-solved/Data-Structures/Strings/Basics/6- rearrange-characters-string-no-two-adjacent/rearrangeString.cpp
+++ geeksforgeeks-solved/Data-Structures/Strings/Basics/6- rearrange-characters-string-no-two-adjacent/rearrangeString.cpp
@@ -8,19 +8,60 @@
  * Note : It may be assumed that the string has only lowercase English alphabets.
  */
 #include <iostream>
+#include <map>
 using namespace std;
 
 string rearrangeString(string s) {
-    
     // TODO: figure this out!, seems like an interesting one
+    map<char, int> mp;
+    for(int i = 0; i < s.size(); i++) {
+        mp[s[i]]++;
+    }
+    
+    int maxCount = 0;
+    int el = 0;
+    for(auto i : mp) {
+        if(i.second > maxCount) {
+            maxCount = i.second;
+            el = i.first;
+        }
+    }
+    
+    int insertCount = 0;
+    for(int i = 0; i < s.size(); i += 2) {
+        s[i] = el;
+        insertCount++;
+    }
+    
+    if(maxCount != insertCount) {
+        return "Not Possible";
+    }
+    
+    int start = 1;
+    for(auto i : mp) {
+        if(i.first != el) {
+            int count = 0;
+            for(int j = start; j < s.size(); j = j+2) {
+                if(count < i.second) {
+                    s[j] = i.first;
+                    count++;
+                }
+                else {
+                    start = j;
+                    break;
+                }
+            }
+
+        }
+    }
+    
     return s;
-           
 }
 
 int main() {
-    string s = "aaabc";
+    string s = "aaaabc";
     cout << rearrangeString(s) << "\n";
     
-   
+    
     return 0;
 }
