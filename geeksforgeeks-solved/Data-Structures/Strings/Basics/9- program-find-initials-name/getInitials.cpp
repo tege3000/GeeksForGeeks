59c31096fd4120ab4d8e34479f45cf139fd85c9b
Tito <teetoegeonu@gmail.com>
2020-10-18 09:59:38 +0100

    UPD - added another approach

--- geeksforgeeks-solved/Data-Structures/Strings/Basics/9- program-find-initials-name/getInitials.cpp
+++ geeksforgeeks-solved/Data-Structures/Strings/Basics/9- program-find-initials-name/getInitials.cpp
@@ -11,27 +11,44 @@
 #include <sstream>
 
 using namespace std;
-
+// Second approach - More intuitive, but less efficient than previous
+// approach
 string getInitials(string s) {
-    vector<string> names;
-    
-    istringstream iss(s);
-    
-    for(string s; iss >> s;) {
-        names.push_back(s);
-    }
-    
-    
     string result;
-    for(int i = 0; i < names.size(); i++) {
-        result.push_back(toupper(names[i][0]));
-        if(i != names.size() - 1) {
+    
+    result.push_back((char)toupper(s[0]));
+    result.push_back(' ');
+    
+    for(int i = 1; i < s.size(); i++) {
+        if(s[i] == ' ') {
+            result.push_back((char)toupper(s[i+1]));
             result.push_back(' ');
         }
     }
     
     return result;
 }
+
+//// Initial approach - Uses istringstream to split the string at spaces
+//string getInitials(string s) {
+//    vector<string> names;
+//
+//    istringstream iss(s);
+//
+//    for(string s; iss >> s;) {
+//        names.push_back(s);
+//    }
+//
+//    string result;
+//    for(int i = 0; i < names.size(); i++) {
+//        result.push_back((char)toupper(names[i][0]));
+//        if(i != names.size() - 1) {
+//            result.push_back(' ');
+//        }
+//    }
+//
+//    return result;
+//}
 
 int main() {
     
