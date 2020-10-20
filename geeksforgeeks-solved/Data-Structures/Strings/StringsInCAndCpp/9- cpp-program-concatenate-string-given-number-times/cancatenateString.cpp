d72600afc7d23e1a2cca64e88c3ffbd8cf080012
Tito <teetoegeonu@gmail.com>
2020-10-20 09:49:57 +0100

    ADD - Implemented algorithm to cancatenate string n times

---
+++ geeksforgeeks-solved/Data-Structures/Strings/StringsInCAndCpp/9- cpp-program-concatenate-string-given-number-times/cancatenateString.cpp
@@ -0,0 +1,23 @@
+/*
+ * Topic: C++ program to concatenate a string given number of times
+ *
+ * Description: Write a function which returns a new string which
+ * is made by concatenating a given string n number of times.
+ */
+#include <iostream>
+using namespace std;
+
+string cancatenateString(int n, string s) {
+    string result = "";
+    
+    for(int i = 0; i < n; i++) {
+        result += s;
+    }
+    
+    return result;
+}
+
+int main() {
+    
+    cout << cancatenateString(3, "geeks")<< "\n";
+}
