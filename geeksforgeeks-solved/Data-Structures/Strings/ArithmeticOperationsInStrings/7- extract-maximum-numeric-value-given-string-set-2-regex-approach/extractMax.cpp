8685d1981faf4b3dfcdd068f47d5b6de3c0ee911
Tito <teetoegeonu@gmail.com>
2020-10-26 10:26:34 +0100

    UPD - Added second approach for  extracting maximum numeric value from a given string

--- geeksforgeeks-solved/Data-Structures/Strings/ArithmeticOperationsInStrings/7- extract-maximum-numeric-value-given-string-set-2-regex-approach/extractMax.cpp
+++ geeksforgeeks-solved/Data-Structures/Strings/ArithmeticOperationsInStrings/7- extract-maximum-numeric-value-given-string-set-2-regex-approach/extractMax.cpp
@@ -6,39 +6,56 @@
  * maximum numeric value from that string.
  */
 #include <iostream>
-#include <algorithm>
+#include <regex>
 using namespace std;
 
-// Initial approach (not using regex approach)
+// Second approach (using regex)
 int extractMax(string s) {
-    string nS;
     int maxEl = 0;
-    int number = 0;
-    for(int i = 0; i < s.size(); i++) {
-        if(isdigit(s[i])) {
-            nS.push_back(s[i]);
+    smatch m;
+    regex r("[0-9]+");
+    while (regex_search(s,m,r)) {
+        for (auto x:m) {
+            maxEl = max(maxEl, stoi(x));
         }
-        else {
-            // takes care of leading numbers i.e 122...
-            if(i != 0) {
-                if(isalpha(s[i]) && isalpha(s[i-1])) {
-                    nS = "";
-                }
-                else {
-                    maxEl = max(maxEl, stoi(nS));
-                    nS = "";
-                }
-            }
-        }
+        
+        // sets s to the remaing string after match
+        s = m.suffix().str();
     }
-    
-    // takes care of trailing numbers  i.e ...123
-    if(isdigit(s[s.size()-1])) {
-        maxEl = max(maxEl, stoi(nS));
-    }
-    
+
     return maxEl;
 }
+
+// // Initial approach (not using regex approach)
+// int extractMax(string s) {
+//     string nS;
+//     int maxEl = 0;
+//     int number = 0;
+//     for(int i = 0; i < s.size(); i++) {
+//         if(isdigit(s[i])) {
+//             nS.push_back(s[i]);
+//         }
+//         else {
+//             // takes care of leading numbers i.e 122...
+//             if(i != 0) {
+//                 if(isalpha(s[i]) && isalpha(s[i-1])) {
+//                     nS = "";
+//                 }
+//                 else {
+//                     maxEl = max(maxEl, stoi(nS));
+//                     nS = "";
+//                 }
+//             }
+//         }
+//     }
+    
+//     // takes care of trailing numbers  i.e ...123
+//     if(isdigit(s[s.size()-1])) {
+//         maxEl = max(maxEl, stoi(nS));
+//     }
+    
+//     return maxEl;
+// }
 
 int main() {
     string s1 = "100klh564abc365bg";
