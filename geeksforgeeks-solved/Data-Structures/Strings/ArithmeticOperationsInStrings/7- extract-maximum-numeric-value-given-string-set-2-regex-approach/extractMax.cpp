d55f1d36250e538bcffcee96a19edaee853db2a5
Tito <teetoegeonu@gmail.com>
2020-10-26 09:39:33 +0100

    ADD - Implemented algorithm for extracting maximum numeric value given in a string

---
+++ geeksforgeeks-solved/Data-Structures/Strings/ArithmeticOperationsInStrings/7- extract-maximum-numeric-value-given-string-set-2-regex-approach/extractMax.cpp
@@ -0,0 +1,49 @@
+/*
+ * Topic: Extract maximum numeric value from a given
+ * string | Set 2 (Regex approach)
+ *
+ * Description: Given an alphanumeric string, extract
+ * maximum numeric value from that string.
+ */
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// Initial approach (not using regex approach)
+int extractMax(string s) {
+    string nS;
+    int maxEl = 0;
+    int number = 0;
+    for(int i = 0; i < s.size(); i++) {
+        if(isdigit(s[i])) {
+            nS.push_back(s[i]);
+        }
+        else {
+            // takes care of leading numbers i.e 122...
+            if(i != 0) {
+                if(isalpha(s[i]) && isalpha(s[i-1])) {
+                    nS = "";
+                }
+                else {
+                    maxEl = max(maxEl, stoi(nS));
+                    nS = "";
+                }
+            }
+        }
+    }
+    
+    // takes care of trailing numbers  i.e ...123
+    if(isdigit(s[s.size()-1])) {
+        maxEl = max(maxEl, stoi(nS));
+    }
+    
+    return maxEl;
+}
+
+int main() {
+    string s1 = "100klh564abc365bg";
+    string s2 = "abchsd0365sdhs";
+    
+    cout << extractMax(s1) << "\n";
+    cout << extractMax(s2) << "\n";
+}
