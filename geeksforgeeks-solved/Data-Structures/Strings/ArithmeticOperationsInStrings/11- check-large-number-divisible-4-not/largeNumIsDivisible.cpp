9cbdf746859d1ef30c9ea5afe0b1b44553018d7d
Tito <teetoegeonu@gmail.com>
2020-10-28 09:14:46 +0100

    ADD - Added justificiation for why the method were using to solve this problem works

--- geeksforgeeks-solved/Data-Structures/Strings/ArithmeticOperationsInStrings/11- check-large-number-divisible-4-not/largeNumIsDivisible.cpp
+++ geeksforgeeks-solved/Data-Structures/Strings/ArithmeticOperationsInStrings/11- check-large-number-divisible-4-not/largeNumIsDivisible.cpp
@@ -16,6 +16,22 @@ using namespace std;
  *
  * A number is divisible by 4 if number formed by last two
  * digits of it is divisible by 4.
+ * 
+ * HOW IT WORKS
+ * Let us consider 76952, we can write it as
+ * 76942 = 7*10000 + 6*1000 + 9*100 + 5*10 + 2
+ * 
+ * The proof is based on below observation:
+ * Remainder of 10i divided by 4 is 0 if i greater than or equal 
+ * to two. Note than 100, 1000, ... etc lead to remainder 0 when
+ *  divided by 4.
+ * 
+ * So remainder of "7*10000 + 6*1000 + 9*100 + 
+ * 5*10 + 2" divided by 4 is equivalent to remainder 
+ * of following : 
+ * 0 + 0 + 0 + 5*10 + 2 = 52
+ * Therefore we can say that the whole number is 
+ * divisible by 4 if 52 is divisible by 4.
  */
 bool isDivisible(string num, int n) {
     string lastTwoDigits;
