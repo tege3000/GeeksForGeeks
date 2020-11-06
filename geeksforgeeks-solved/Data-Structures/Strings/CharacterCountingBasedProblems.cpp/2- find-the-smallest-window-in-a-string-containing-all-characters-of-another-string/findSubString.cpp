44a3ac8bf2698df0f1855cf0e0aa55f2f0b64c0c
Tito <teetoegeonu@gmail.com>
2020-11-06 11:34:09 +0100

    ADD - Implemented algorithm to find the smallest window in substring

---
+++ geeksforgeeks-solved/Data-Structures/Strings/CharacterCountingBasedProblems.cpp/2- find-the-smallest-window-in-a-string-containing-all-characters-of-another-string/findSubString.cpp
@@ -0,0 +1,57 @@
+/*
+ * Topic: Find the smallest window in a string containing all
+ * characters of another string
+ *
+ * Description: Given two strings string1 and string2, the task
+ * is to find the smallest substring in string1 containing all
+ * characters of string2 efficiently.
+ */
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+string findSubString(string s, string p) {
+    int minSubs = INT_MAX;
+    string ans = "";
+    for(int i = 0; i < s.size(); i++) {
+        string sub = "";
+        for(int j = i; j < s.size(); j++) {
+            sub.push_back(s[j]);
+            string subCopy = sub;
+            int allFound = 1;
+            for(int k = 0; k < p.size(); k++) {
+                auto it = find(subCopy.begin(), subCopy.end(), p[k]);
+            
+                if(it != subCopy.end()) {
+                    int pos = distance(subCopy.begin(), it);
+                    subCopy.erase(subCopy.begin()+pos);
+                }
+                else {
+                    allFound = 0;
+                    break;
+                }
+            }
+
+            if(allFound == 1){
+                if(sub.size() < minSubs) {
+                    minSubs = sub.size();
+                    ans = sub;
+                }
+            }
+            
+        }
+    }
+    
+    return ans;
+}
+
+int main() {
+    string str = "this is a test string";
+    string pat = "tist";
+    cout << "Smallest window is : " << findSubString(str, pat) << "\n";
+    
+    string str2 = "geeksforgeeks";
+    string pat2 = "ork";
+    cout << "Smallest window is : " << findSubString(str2, pat2) << "\n";
+}
+
