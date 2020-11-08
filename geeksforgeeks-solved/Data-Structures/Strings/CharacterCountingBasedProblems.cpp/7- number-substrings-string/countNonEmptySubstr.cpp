3b693fb1765250caee34402655a854f059bf68a4
Tito <teetoegeonu@gmail.com>
2020-11-08 09:37:13 +0100

    UPD - Added efficient approach to find the number of substrings in a string

--- geeksforgeeks-solved/Data-Structures/Strings/CharacterCountingBasedProblems.cpp/7- number-substrings-string/countNonEmptySubstr.cpp
+++ geeksforgeeks-solved/Data-Structures/Strings/CharacterCountingBasedProblems.cpp/7- number-substrings-string/countNonEmptySubstr.cpp
@@ -8,17 +8,28 @@
 #include <iostream>
 using namespace std;
 
+// Efficient approach
+// Works in O(n) time
+// Count of non-empty substrings is n*(n+1)/2
 int countNonEmptySubstr(string s) {
-    int count = 0;
+    int n = s.size();
     
-    for(int i = 0; i < s.size(); i++) {
-        for(int j = i; j < s.size(); j++) {
-            count++;
-        }
-    }
-    
-    return count;
+    return n*(n+1)/2;
 }
+
+// // first approach
+// // Works in O(n^2) time
+// int countNonEmptySubstr(string s) {
+//     int count = 0;
+
+//     for(int i = 0; i < s.size(); i++) {
+//         for(int j = i; j < s.size(); j++) {
+//             count++;
+//         }
+//     }
+
+//     return count;
+// }
 
 int main() {
     string s = "abcd";
