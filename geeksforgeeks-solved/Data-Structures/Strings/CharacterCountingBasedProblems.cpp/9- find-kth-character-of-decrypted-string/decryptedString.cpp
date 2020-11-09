10d98a5b374d76d594645caf2d63842a95ba0a02
Tito <teetoegeonu@gmail.com>
2020-11-09 11:12:50 +0100

    ADD - Started implementing algorithm to find kth character of decrypted string

---
+++ geeksforgeeks-solved/Data-Structures/Strings/CharacterCountingBasedProblems.cpp/9- find-kth-character-of-decrypted-string/decryptedString.cpp
@@ -0,0 +1,51 @@
+/*
+ * Topic: Find k’th character of decrypted string | Set 1
+ *
+ * Description: Given an encoded string where repetitions of
+ * substrings are represented as substring followed by count
+ * of substrings. For example, if encrypted string is “ab2cd2”
+ * and k=4 , so output will be ‘b’ because decrypted string is
+ * “ababcdcd” and 4th character is ‘b’.
+ *
+ * Note: Frequency of encrypted substring can be of more than
+ * one digit. For example, in “ab12c3”, ab is repeated 12 times.
+ * No leading 0 is present in frequency of substring.
+ */
+#include <iostream>
+using namespace std;
+
+string decryptedString(string s, int k) {
+    string res;
+    string digits;
+    int firstIndex = 0;
+    int lastIndex = 0;
+    int digitLen = 0;
+    for(int i = 0; i < s.size(); i++) {
+        if(isdigit(s[i]) ) {
+            for(int j = i; j < s.size(); j++) {
+                if(!isdigit(s[j])) {
+                    break;
+                }
+                digits.push_back(s[j]);
+                digitLen++;
+            }
+            
+            for(int j = 0; j < (s[i]-'0'); j++) {
+                res.insert(res.size()-1, s.substr(firstIndex, i));
+            }
+            
+            lastIndex = 0;
+            firstIndex = i + digitLen;
+        }
+        lastIndex++;
+    }
+    
+    return res;
+}
+
+int main() {
+    string s = "ab4c12ed3";
+    int k = 21;
+    cout << decryptedString(s, k) << "\n";
+}
+
