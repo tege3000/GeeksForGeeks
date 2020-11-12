b2757e748fc2842d4bbd46183cb1659342a54767
Tito <teetoegeonu@gmail.com>
2020-11-12 16:15:05 +0100

    UPD - Implemented space efficient solution

--- geeksforgeeks-solved/Data-Structures/Strings/CharacterCountingBasedProblems.cpp/14- count-substrings-with-same-first-and-last-characters/countSubstringWithEqualEnds.cpp
+++ geeksforgeeks-solved/Data-Structures/Strings/CharacterCountingBasedProblems.cpp/14- count-substrings-with-same-first-and-last-characters/countSubstringWithEqualEnds.cpp
@@ -5,28 +5,45 @@
  * of all contiguous substrings starting and ending with same character.
  */
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
+// Space Efficient Method
 int countSubstringWithEqualEnds(string s) {
     int count = 0;
-    int initSize = s.size();
     
+    // The key is to iterate through the elements in the substring in a 
+    // way to get the first and last elements easily.
     for(int i = 0; i < s.size(); i++) {
-        for(int j = 1; j <= initSize; j++) {
-            string subs = s.substr(i, j);
-            
-            if(subs[0] == subs[subs.size()-1]) {
+        for(int j = i; j < s.size(); j++) {
+            if(s[i] == s[j]) {
                 count++;
             }
         }
         
-        initSize--;
     }
     
     return count;
 }
+
+// brute force solution
+//int countSubstringWithEqualEnds(string s) {
+//    int count = 0;
+//    int initSize = s.size();
+//
+//    for(int i = 0; i < s.size(); i++) {
+//        for(int j = 1; j <= initSize; j++) {
+//            string subs = s.substr(i, j);
+//
+//            if(subs[0] == subs[subs.size()-1]) {
+//                count++;
+//            }
+//        }
+//
+//        initSize--;
+//    }
+//
+//    return count;
+//}
 
 int main() {
     string s = "abcab";
