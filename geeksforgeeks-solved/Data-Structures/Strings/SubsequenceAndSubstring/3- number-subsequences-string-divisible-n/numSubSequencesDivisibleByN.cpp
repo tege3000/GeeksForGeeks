c58597b483c5754a6c7d17eb7a3b276b7a84bdc0
Tito <teetoegeonu@gmail.com>
2020-11-18 13:00:35 +0100

    UPD - finally implemented an algorithm for this problem

--- geeksforgeeks-solved/Data-Structures/Strings/SubsequenceAndSubstring/3- number-subsequences-string-divisible-n/numSubSequencesDivisibleByN.cpp
+++ geeksforgeeks-solved/Data-Structures/Strings/SubsequenceAndSubstring/3- number-subsequences-string-divisible-n/numSubSequencesDivisibleByN.cpp
@@ -9,11 +9,9 @@
 #include <math.h>
 using namespace std;
 
-// TODO: Finish up later,
-// Current Idea: Trying to use Truth Tables to get all subsequences
-int numSubSequencesDivisibleByN(string s, int n) {
-    // get all subsequences first
-    vector<int> subsequences;
+// for a string of numbers, get all subsequences
+vector <string> getAllSubsequences(string s) {
+    vector<string> subsequences;
     
     // for n characters, there are 2^n possibilities
     int numSubs = pow(2, s.size());
@@ -27,45 +25,73 @@ int numSubSequencesDivisibleByN(string s, int n) {
         truthTable.push_back(inner);
     }
     
-    
-    // need to fix this part of code. Not populating truth table as intended
-    int start = 0;
+    // generate truth table
     for(int i = 0; i < s.size(); i++) {
-        int numRecurring = numSubs / pow(2, s.size()-(i+1));
-        
-        for(int j = 0; j < numRecurring; j++) {
-            start = 0;
+        int numRecurring = pow(2, s.size()-(i+1));
+        int start = 0;
+        int end = numRecurring;
+        for(int j = 0; j < pow(2, i+1); j++) {
+            end = numRecurring + start;
             if(j % 2 == 0) {
                 // fill up with zeros
-                for(int k = start; k < (start+pow(2, s.size()-(i+1))); k++) {
+                for(int k = start; k < end; k++) {
                     truthTable[k][i] = 0;
                 }
             }
             else {
-                start += pow(2, s.size()-(i+1));
                 // fill up with 1s
-                for(int k = start; k < (start+pow(2, s.size()-(i+1))); k++) {
+                for(int k = start; k < end; k++) {
                     truthTable[k][i] = 1;
                 }
             }
+            
+            start += numRecurring;
         }
-
     }
     
     for(int i = 0; i < numSubs; i++) {
+        string num = "";
         for(int j = 0; j < s.size(); j++) {
-            cout << truthTable[i][j] << " ";
+            if(truthTable[i][j] == 1) {
+                num += s[j];
+            }
         }
-        cout << "\n";
+        
+        if(num.size() >= 1) {
+            subsequences.push_back(num);
+        }
     }
     
-    return 0;
+    return subsequences;
+}
+
+
+int numSubSequencesDivisibleByN(string s, int n) {
+    // get all subsequences first
+    vector<string> allSubsequences = getAllSubsequences(s);
+    
+    int count = 0;
+    for(int i = 0; i < allSubsequences.size(); i++) {
+        if(stoi(allSubsequences[i]) % n == 0) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 int main() {
     string str = "1234";
     int n = 4;
-    cout << numSubSequencesDivisibleByN(str, n);
+    cout << numSubSequencesDivisibleByN(str, n) << "\n";
+    
+    string str2 = "330";
+    int n2 = 6;
+    cout << numSubSequencesDivisibleByN(str2, n2) << "\n";
+    
+    string str3 = "676";
+    int n3 = 6;
+    cout << numSubSequencesDivisibleByN(str3, n3) << "\n";
     
     return 0;
 }
