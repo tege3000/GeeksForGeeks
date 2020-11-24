475ba5b1743240726062b552041cc642070c0c2e
Tito <teetoegeonu@gmail.com>
2020-11-24 16:11:54 +0100

    ADD - Finally implemented algorithm for sorting a dictionary and finding precedence characters

--- geeksforgeeks-solved/Data-Structures/Strings/SortingAndSearching.cpp/3- given-sorted-dictionary-find-precedence-characters/printOrder.cpp
+++ geeksforgeeks-solved/Data-Structures/Strings/SortingAndSearching.cpp/3- given-sorted-dictionary-find-precedence-characters/printOrder.cpp
@@ -7,15 +7,33 @@
  */
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-void printOrder(vector<string> arr, int n) {
+void printOrder(vector<string> arr) {
     vector<char> sorted;
+    int maxLen = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        int len = arr[i].size();
+        maxLen = max(len, maxLen);
+    }
+    
     for(int i = 0; i < arr.size()-1; i++) {
         for(int j = 0; j < arr.size(); j++) {
-            if(arr[i][j] < arr[i+1][j]) {
-                if((find(sorted.begin(), sorted.end(), arr[i+1][j]) != sorted.end())) {
-                    sorted.insert(sorted.begin()+distance(sorted.begin(), find(sorted.begin(), sorted.end(), arr[i+1][j]))+1, arr[i][j]);
+            if(arr[i][j] != arr[i+1][j]) {
+                if(sorted.size() == maxLen) {
+                    int pos1 = distance(sorted.begin(), find(sorted.begin(), sorted.end(), arr[i][j]));
+                    int pos2 = distance(sorted.begin(), find(sorted.begin(), sorted.end(), arr[i+1][j]));
+                    
+                    if(pos1 > pos2) {
+                        swap(sorted[pos1], sorted[pos2]);
+                    }
+                }
+                else if((find(sorted.begin(), sorted.end(), arr[i+1][j]) != sorted.end())) {
+                    sorted.insert(sorted.begin()+distance(sorted.begin(), find(sorted.begin(), sorted.end(), arr[i+1][j])), arr[i][j]);
+                }
+                else if((find(sorted.begin(), sorted.end(), arr[i][j]) != sorted.end())) {
+                    sorted.insert(sorted.begin()+distance(sorted.begin(), find(sorted.begin(), sorted.end(), arr[i][j]))+1, arr[i+1][j]);
                 }
                 else {
                     sorted.push_back(arr[i][j]);
@@ -36,7 +54,11 @@ void printOrder(vector<string> arr, int n) {
 
 int main() {
     vector<string> words = {"baa", "abcd", "abca", "cab", "cad"};
-    printOrder(words, 3);
+    printOrder(words);
+    
+    vector<string> words2 = {"caa", "aaa", "aab"};
+    printOrder(words2);
+    
     return 0;
 }
 
